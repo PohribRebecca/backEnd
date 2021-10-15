55970a2ef5604c26544d509ef24a9aa628a5a771
Pohrib Rebecca <pohribrebecca1@gmail.com>
2021-10-15 17:31:09 +0300

    Add project files.

---
+++ backEnd/Controllers/ProdusController.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using backEnd.Models;
+using Dapper;
+
+
+namespace backEnd.Controllers
+{
+    //[Route("api/produs")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdusController : ControllerBase
+    {
+
+        private readonly IConfiguration _configuration;
+
+        public ProdusController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        //string query = @"select * from dbo.Produs where ProdusId=@ProdusId";
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var ProdusId = 1;
+            string query = @"select * from dbo.Produs";
+            string sqlDataSrouce = _configuration.GetConnectionString("ProductAppCon");
+            using (SqlConnection myCon = new SqlConnection(sqlDataSrouce))
+            {
+                myCon.Open();
+
+                return this.Ok(myCon.Query<Produs>(query, new { ProdusId }).ToList());
+
+
+            }
+
+
+
+
+            //    string query = @"select * from dbo.Produs";
+            //DataTable table = new DataTable();
+            //string sqlDataSrouce = _configuration.GetConnectionString("ProductAppCon");
+            //SqlDataReader myReader;
+            //using (SqlConnection myCon = new SqlConnection(sqlDataSrouce))
+            //{
+            //    myCon.Open();
+            //    using (SqlCommand myComamand = new SqlCommand(query, myCon))
+
+            //    {
+            //        myReader = myComamand.ExecuteReader();
+            //        table.Load(myReader);
+            //        myReader.Close();
+            //        myCon.Close();
+            //    }
+            //}
+            //return new JsonResult(table);
+        }
+
+
+
+        [HttpPut]
+        public JsonResult Put([FromBody]Produs prod)
+        {
+            string query = @"
+                     update dbo.Produs set   
+                      NumeProdus = @NumeProdus
+                     ,Categorie = @Categorie
+                     ,Producator = @Producator
+                     ,Pret = @Pret
+                     ,Stoc = @Stoc
+                     where ProdusId = @ProdusId";
+
+
+            string sqlDataSrouce = _configuration.GetConnectionString("ProductAppCon");
+            using (SqlConnection myCon = new SqlConnection(sqlDataSrouce))
+            {
+                myCon.Open();
+
+
+                myCon.Execute(query,  prod);
+
+
+
+                //using (SqlCommand myComamand = new SqlCommand(query, myCon))
+                //{
+                //    myComamand.CommandType = CommandType.Text;
+                //    myComamand.ExecuteNonQuery();
+                //}
+                //myCon.Close();
+            }
+            return new JsonResult("Updated Successfully");
+        }
+
+
+
+    }
+}
---
+++ backEnd/Models/Produs.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+/*CREATE TABLE Produs (
+ProdusId int NOT NULL IDENTITY(1,1) PRIMARY KEY,
+NumeProdus varchar(200), 
+Categorie varchar(200),
+Producator varchar(200),
+Pret int,
+Stoc int
+);*/
+
+
+namespace backEnd.Models
+{
+    public class Produs
+    {
+        public int ProdusId { get; set; }
+        //[Required]
+        public string NumeProdus { get; set; }
+        public string Categorie { get; set; }
+        public string Producator { get; set; }
+        public float Pret { get; set; }
+        public int Stoc { get; set; }
+
+    }
+}
